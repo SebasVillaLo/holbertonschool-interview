0c8edec97c4903db21aeb506be564cb3ee002263
SebasVillaLo <villalopezsebastian@gmail.com>
2022-04-21 16:34:38 -0500

    fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 def canUnlockAll(boxes):
+    """sumary_line
+
+    Keyword arguments:
+    argument -- description
+    Return: return_description
+    """
+
     unlocked = [0]
+
     for index_box, value_box in enumerate(boxes):
-        # if not value_box:
-        #     continue
+        if not value_box:
+            continue
         for key in value_box:
             if key < len(boxes) and key not in unlocked and key != index_box:
                 unlocked.append(key)
+
     if len(unlocked) == len(boxes):
         return True
     return False
