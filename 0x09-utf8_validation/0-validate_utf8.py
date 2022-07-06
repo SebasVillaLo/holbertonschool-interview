d7551af6c83a828d1c9c7b96ba8c53b082fd4a90
SebasVillaLo <villalopezsebastian@gmail.com>
2022-07-06 15:43:35 -0500

    add all task

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,48 @@
-#!/usr/bin/python3
-"""
-Defines a method that determines if a given data set
-represents valid UTF-8 encoding
-"""
+def ordenar(doc_id, nombre, apellidos, telefono):
+    for i in range(0, N-1):
+        for j in range(i+1, N):
+            if nombre[i] > nombre[j]:
+                # organizo doc_id
+                t = doc_id[i]
+                doc_id[i] = doc_id[j]
+                doc_id[j] = t
+                # organizo nombre
+                t = nombre[i]
+                nombre[i] = nombre[j]
+                nombre[j] = t
+                # organizo apellidos
+                t = apellidos[i]
+                apellidos[i] = apellidos[j]
+                apellidos[j] = t
+                # organizo telefono
+                t = telefono[i]
+                telefono[i] = telefono[j]
+                telefono[j] = t
+                return doc_id, nombre, apellidos, telefono
 
 
-def validUTF8(data):
-    """
-    Determines if a given data set represents valid UTF-8 encoding
-    parameters:
-        data [list of ints]:
-            each integer represents 1 byte of data
-            data set can contain multiple characters
-            each character in UTF-8 can be 1 to 4 bytes long
-    returns:
-        True if data is valid UTF-8 encoding, False otherwise
-    """
-    if type(data) is not list:
-        return False
-    for i in data:
-        if type(i) is not int:
-            return False
-    return True
+N = int(input('Ingrese cantidad de datos a registrar: '))
+doc_id = []
+nombre = []
+apellidos = []
+telefono = []
+for i in range(N):
+    doc_id.append(int(input('ingrese documento de identidad: ')))
+    nombre.append(input('ingrese nombre: '))
+    apellidos.append(input('apellido: '))
+    telefono.append(int(input('Telefono: ')))
+    print()
+    print(doc_id)
+    print(nombre)
+    print(apellidos)
+    print(telefono)
+    documento, nombres, apellido, telefonos = ordenar(
+        doc_id, nombre, apellidos, telefono)
+    print()
+    print()
+for x in range(N):
+    print(documento[x])
+    print(nombres[x])
+    print(apellido[x])
+    print(telefonos[x])
+    print()
